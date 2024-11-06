4bc79463622129b372dc54854565d769be42d86a
denysyen <102578519+denysyen@users.noreply.github.com>
2024-11-06 23:48:50 +0100

    Initial navegation v1

--- Car_Rental_project/car_rental_frontend/src/app/app.component.ts
+++ Car_Rental_project/car_rental_frontend/src/app/app.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { NgZorroImportsModule } from './NgZorroImportsModule';
-
-
+import { StorageService } from './auth/services/storage/storage.service';
+import { CommonModule } from '@angular/common';  
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgZorroImportsModule],
+  imports: [RouterOutlet, NgZorroImportsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'car_rental_ui';
+  
+  isCustomerLoggedIn: boolean = StorageService.isCustomerLoggedIn();
+  isAdminLoggedIn: boolean = StorageService.isAdminLoggedIn();
+
+  constructor(private router: Router) {}
+  
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if(event.constructor.name == "NavigationEnd") {
+        this.isAdminLoggedIn = StorageService.isAdminLoggedIn();
+        this.isCustomerLoggedIn = StorageService.isCustomerLoggedIn();
+      }      
+    })
+  }
+
+  logout() {
+    StorageService.logout();
+    this.router.navigateByUrl("/login");
+  }
 }
 
--- Car_Rental_project/car_rental_frontend/src/app/auth/services/storage/storage.service.ts
+++ Car_Rental_project/car_rental_frontend/src/app/auth/services/storage/storage.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from "@angular/core";
+import { Inject, Injectable, InjectionToken } from "@angular/core";
 
 const TOKEN = "token";
 const USER = "user";
-
+  
 @Injectable({
     providedIn: 'root'
 })
 
 export class StorageService {
     constructor() {}
-
+    
     static saveToken(token: string): void {
         window.localStorage.removeItem(TOKEN);
         window.localStorage.setItem(TOKEN, token);
@@ -20,7 +20,14 @@ export class StorageService {
         window.localStorage.setItem(USER, JSON.stringify(user));
     }
     static getToken() {
-        return window.localStorage.getItem(TOKEN);
+        var getTokenFromStorage;
+        // need to double check window object because I am  
+        // running an app in server side while window object
+        // come from the browser side, need to avoid pre-render issues 
+        if (typeof window !== 'undefined') {
+            getTokenFromStorage = localStorage ? localStorage.getItem(USER) : null;
+        }
+        return getTokenFromStorage || null;
     }
     static getUser() {
         return JSON.parse(localStorage.getItem(USER) || '{}');
@@ -42,6 +49,11 @@ export class StorageService {
         if(this.getToken() == null) return false;
         const role: string = this.getUserRole();
         return role == "CUSTOMER";
+    }
+
+    static logout(): void {
+        window.localStorage.removeItem(TOKEN);
+        window.localStorage.removeItem(USER);
     }
 
 }
