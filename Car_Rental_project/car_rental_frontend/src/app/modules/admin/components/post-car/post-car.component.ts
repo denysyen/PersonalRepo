a31cfe092b1430a18cbc8d8f97a1dfed2dbb30a1
denysyen <102578519+denysyen@users.noreply.github.com>
2024-11-06 22:19:56 +0100

    Initial navegation v1

---
+++ Car_Rental_project/car_rental_frontend/src/app/modules/admin/components/post-car/post-car.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-car',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-car.component.html',
+  styleUrl: './post-car.component.css'
+})
+export class PostCarComponent {
+
+}
