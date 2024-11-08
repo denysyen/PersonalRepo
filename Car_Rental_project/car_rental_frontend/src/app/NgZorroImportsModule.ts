e177057dd356e4c2af6d6a2505e7da4e200e93fe
denysyen <102578519+denysyen@users.noreply.github.com>
2024-11-08 21:26:04 +0100

    Car form component  creation

--- Car_Rental_project/car_rental_frontend/src/app/NgZorroImportsModule.ts
+++ Car_Rental_project/car_rental_frontend/src/app/NgZorroImportsModule.ts
@@ -4,14 +4,17 @@ import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
-
+import { NzSelectModule } from 'ng-zorro-antd/select'
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 
 @NgModule({
     exports: [ NzSpinModule,
         NzFormModule,
         NzButtonModule,
         NzInputModule,
-        NzLayoutModule]
+        NzLayoutModule,
+        NzSelectModule,
+        NzDatePickerModule]
 })
 
 export class NgZorroImportsModule {
--- Car_Rental_project/car_rental_frontend/src/app/modules/admin/admin.module.ts
+++ Car_Rental_project/car_rental_frontend/src/app/modules/admin/admin.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { PostCarComponent } from './components/post-car/post-car.component';
+import { NgZorroImportsModule } from '../../NgZorroImportsModule';
+import {  ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminDashboardComponent,
+    PostCarComponent
+  ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    NgZorroImportsModule,
+    ReactiveFormsModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class AdminModule { }
