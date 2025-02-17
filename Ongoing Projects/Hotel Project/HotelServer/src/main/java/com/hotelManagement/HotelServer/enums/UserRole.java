ba16c20df0dad807fb66db8e06fd4c06bc76f913
denysyen <102578519+denysyen@users.noreply.github.com>
2025-02-17 18:18:31 +0100

    Delete Ongoing Projects/Hotel Project directory

--- Ongoing Projects/Hotel Project/HotelServer/src/main/java/com/hotelManagement/HotelServer/enums/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.hotelManagement.HotelServer.enums;
-
-public enum UserRole {
-    ADMIN,
-    CUSTOMER
-}
--- Ongoing Projects/Hotel Project/HotelServer/src/main/java/com/hotelManagement/HotelServer/service/jwt/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.hotelManagement.HotelServer.service.jwt;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService {
-    UserDetailsService userDetailsService();
-
-}
